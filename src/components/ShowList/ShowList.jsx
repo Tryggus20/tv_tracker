cbc92ccc07d8a40c022dec0c6b82ffccfda50728
David Campbell <tryggus20@gmail.com>
2024-05-28 11:16:51 -0500

    added tvmaze api

--- src/components/ShowList/ShowList.jsx
+++ src/components/ShowList/ShowList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ShowContainer from "../ShowContainer/ShowContainer";
 import {
@@ -11,29 +11,48 @@ import {
   TableCell,
   TableBody,
 } from "@mui/material";
+import axios from "axios";
 
 function ShowList() {
   const dispatch = useDispatch();
 
-  // TODO: may need to change store name
   let { shows } = useSelector((store) => store);
 
   useEffect(() => {
     dispatch({ type: "FETCH_SHOWS" });
   }, [dispatch]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showDetails, setShowDetails] = useState(null);
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchClick = async () => {
+    try {
+      const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchQuery}`);
+      if (response.data.length > 0) {
+        setShowDetails(response.data[0].show);
+      } else {
+        alert('No show found with that name');
+      }
+    } catch (error) {
+      console.error("Error fetching show details:", error);
+    }
+  };
+//  *********______*********_________*********______*********_________*********______*********_________
   return (
     <div>
-
       <br />
-
       <TextField
         label="Show Name"
+        value={searchQuery}
+        onChange={handleSearchChange}
         size="small"
         variant="outlined"
         sx={{ mr: 2 }} 
       />
-      <Button variant="contained" color="secondary">
+      <Button variant="contained" color="secondary" onClick={handleSearchClick}>
         Add New Show
       </Button>
       <br />
@@ -56,7 +75,13 @@ function ShowList() {
         </Table>
       </Paper>
       <hr />
-      {/* <pre>;{JSON.stringify(shows, null, 2)}</pre> */}
+      {showDetails && (
+        <div>
+          <h3>{showDetails.name}</h3>
+          <p dangerouslySetInnerHTML={{ __html: showDetails.summary }} />
+          <img src={showDetails.image?.medium} alt={showDetails.name} />
+        </div>
+      )}
     </div>
   );
 }
