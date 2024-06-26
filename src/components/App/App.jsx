6ba808869985cd4fc4e65cc32d708765cd0847e3
David Campbell <tryggus20@gmail.com>
2024-06-26 11:36:47 -0500

    fixed chatbox

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -47,7 +47,7 @@ function App() {
   return (
     <AuthProvider>
       <AppContent />
-      <iframe src="https://tvtrackerbot-6854.chipp.ai" height="550px" width="80%" frameborder="0" title="Tv Tracker bot"/>
+      <ChatWidget />
 
     </AuthProvider>
   );
