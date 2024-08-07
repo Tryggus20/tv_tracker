3e9585cbdb9a92d6ce80bebf1c44dac1608f06a1
David Campbell <tryggus20@gmail.com>
2024-08-07 09:42:50 -0500

    styled login and register

--- functions/modules/pool.js
+++ functions/modules/pool.js
@@ -1,13 +1,63 @@
+const pg = require('pg');
+
+// const pool = new Pool({
+//   host: PGHOST,
+//   database: PGDATABASE,
+//   username: PGUSER,
+//   password: PGPASSWORD,
+//   port: 5432,
+//   ssl: {
+//     require: true,
+//   },
+// });
+// async function getPgVersion() {
+//   const client = await pool.connect();
+//   try {
+//     const result = await client.query('SELECT version()');
+//     console.log(result.rows[0]);
+//   } finally {
+//     client.release();
+//   }
+// }
+// getPgVersion();
+
 const { Pool } = require('pg');
-const functions = require('firebase-functions');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: functions.config().pg.user,
-  host: functions.config().pg.host,
-  database: functions.config().pg.database,
-  password: functions.config().pg.password,
-  port: functions.config().pg.port,
+    connectionString: process.env.database_url,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-console.log(functions.config().pg);
+
+pool.on('connect', () => {
+    console.log('Connected to the database');
+});
+
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+module.exports = pool;
+
+// } else if (process.env.GCLOUD_SQL_CONNECTION_NAME) {
+//   // This is for Google Cloud SQL
+//   pool = new pg.Pool({
+//     user: process.env.DB_USER,
+//     host: `/cloudsql/${process.env.GCLOUD_SQL_CONNECTION_NAME}`, // Cloud SQL instance connection name
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: 5432, // Default PostgreSQL port
+//   });
+// } else {
+//   // This is for local development
+//   pool = new pg.Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'tv_tracker',
+//   });
+// }
 
 module.exports = pool;
--- functions/routes/tv_router.js
+++ functions/routes/tv_router.js
@@ -16,6 +16,22 @@ router.get("/", (req, res) => {
     });
 });
 
+// TODO: REMOVE TEST GET TO MAKE SURE DEPLOYED DB IS WORKING
+// router.get("/", (req, res) => {
+//   const userEmail = req.headers['user-email']
+//   console.log(userEmail);;
+//   const queryText = 'SELECT * FROM "shows" ORDER BY show_name ASC';
+
+//   pool
+//     .query(queryText)
+//     .then((result) => res.send(result.rows))
+//     .catch((err) => {
+//       console.error("Error in Get user shows", err);
+//       res.sendStatus(500);
+//     });
+// });
+
+
 // POST ROUTE
 router.post("/", (req, res) => {
   const {
@@ -32,7 +48,7 @@ router.post("/", (req, res) => {
     show_synopsis,
     user_email,
   } = req.body;
-  const queryText = `INSERT INTO "shows" (show_name, season, episode, genre, notes, series_ended, is_completed, last_updated, tvmaze_id, image_url, show_synopsis, user_email) 
+  const queryText = ` INSERT INTO "shows" (show_name, season, episode, genre, notes, series_ended, is_completed, last_updated, tvmaze_id, image_url, show_synopsis, user_email ) 
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
 
   const values = [
@@ -59,28 +75,51 @@ router.post("/", (req, res) => {
     });
 });
 
-// PUT ROUTE
+//PUT ROUTE
+// router.put("/:id", (req, res) => {
+//   const showId = req.params.id;
+//   const updatedShow = req.body;
+//   const queryText = `UPDATE "shows" SET "show_name" = $1, "season" = $2, "episode" = $3, "genre" = $4, "notes" = $5, "series_ended" = $6, "is_completed" = $7, "last_updated" = $8 `;
+//   const values = [
+//     updatedShow.show_name,
+//     updatedShow.season,
+//     updatedShow.episode,
+//     updatedShow.genre,
+//     updatedShow.notes,
+//     updatedShow.series_ended,
+//     updatedShow.is_completed,
+//     updatedShow.last_updated,
+//   ];
+//   pool
+//     .query(queryText, values)
+//     .then(() => res.sendStatus(204))
+//     .catch((err) => {
+//       console.error("Error in Put update show", err);
+//       res.sendStatus(500);
+//     });
+// });
+
 router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { season, episode } = req.body;
-  try {
-    if (season !== undefined) {
-      await pool.query('UPDATE shows SET season = $1 WHERE id = $2', [season, id]);
+    const { id } = req.params;
+    const { season, episode } = req.body;
+    try {
+      if (season !== undefined) {
+        await pool.query('UPDATE shows SET season = $1 WHERE id = $2', [season, id]);
+      }
+      if (episode !== undefined) {
+        await pool.query('UPDATE shows SET episode = $1 WHERE id = $2', [episode, id]);
+      }
+      res.sendStatus(200);
+    } catch (error) {
+      console.error('Error updating show:', error);
+      res.sendStatus(500);
     }
-    if (episode !== undefined) {
-      await pool.query('UPDATE shows SET episode = $1 WHERE id = $2', [episode, id]);
-    }
-    res.sendStatus(200);
-  } catch (error) {
-    console.error('Error updating show:', error);
-    res.sendStatus(500);
-  }
-});
+  });
 
 // DELETE ROUTE
 router.delete("/:id", (req, res) => {
   const showId = req.params.id;
-  const queryText = `DELETE FROM "shows" WHERE "id" = $1`;
+  const queryText = `DELETE "shows" where "id" = $1`;
 
   pool
     .query(queryText, [showId])
